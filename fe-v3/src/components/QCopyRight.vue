341963badb0f0b682211bd0752b7e5b722daa627
Kyle <aw123e@126.com>
2020-12-16 09:16:57 +0800

    remove php

--- fe-v3/src/components/QCopyRight.vue
+++ fe-v3/src/components/QCopyRight.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="copy-right mt-5">
-    &copy; 2018 - {{ nowYear }} 仓颉公版图书典藏计划版权所有
+    &copy; 2018 - {{ nowYear }} 仓颉公版图书典藏计划版权所有, By 伴读
   </div>
 </template>
 <script>
--- fe-v3/src/components/QMonthReadCard.vue
+++ fe-v3/src/components/QMonthReadCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main-tit">壹号月读</div>
   <div class="text">让温暖的笔触抚慰你孤独的心灵</div>
-  <div class="cover-article-small" style="background-image: url('');">
+  <div class="cover-article-small" style="background-image: url('');" />
 </template>
 <script>
 export default {
